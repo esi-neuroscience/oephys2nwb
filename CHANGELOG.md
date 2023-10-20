370439d7a3af86758b9c65e4623710083790c394
Stefan Fuertinger <stefan.fuertinger@esi-frankfurt.de>
2023-10-20 16:58:22 +0200

    CHG: House-keeping update for upcoming release
    - make it possible to convert files w/o specific event channel
      info
    - updated build requirements and dependencies
    - included licensing info
    
     On branch dev
    Changes to be committed:
    	modified:   .gitignore
    	modified:   .gitlab-ci.yml
    	modified:   CHANGELOG.md
    	modified:   CITATION.cff
    	modified:   LICENSE
    	new file:   LICENSES/BSD-3-Clause.txt
    	new file:   LICENSES/CC-BY-NC-SA-1.0.txt
    	new file:   LICENSES/CC0-1.0.txt
    	modified:   README.md
    	modified:   oephys2nwb-dev.yml
    	modified:   oephys2nwb.yml
    	modified:   oephys2nwb/__init__.py
    	modified:   oephys2nwb/__main__.py
    	modified:   oephys2nwb/export2nwb.py
    	modified:   pyproject.toml
    	modified:   setup.cfg
    	modified:   setup.py
    	modified:   tests/script_local.py

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,21 @@
+ <!--
+ Copyright (c) 2023 Ernst Strüngmann Institute (ESI) for Neuroscience
+ in Cooperation with Max Planck Society
+ SPDX-License-Identifier: CC-BY-NC-SA-1.0
+ -->
+
 # Changelog of oephys2nwb
 All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project follows [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+### NEW
+### CHANGED
+### REMOVED
+### DEPRECATED
+### FIXED
 
 ## [2022.5rc0] - 2022-05-20
 First release on PyPI
--- tests/script_local.py
+++ tests/script_local.py
@@ -1,6 +1,11 @@
-# -*- coding: utf-8 -*-
 #
 # Simple script for testing exporter w/o pip-installing it
+#
+#
+# Copyright © 2023 Ernst Strüngmann Institute (ESI) for Neuroscience
+# in Cooperation with Max Planck Society
+#
+# SPDX-License-Identifier: BSD-3-Clause
 #
 
 # Builtin/3rd party package imports
@@ -10,9 +15,10 @@ from pynwb import NWBHDF5IO
 # Add acme to Python search path
 import os
 import sys
-pkg_path = os.path.abspath(".." + os.sep + "..")
+pkg_path = os.path.abspath("..")
 if pkg_path not in sys.path:
     sys.path.insert(0, pkg_path)
+import getpass
 
 # Import package
 from oephys2nwb import export2nwb
@@ -21,16 +27,11 @@ from oephys2nwb import export2nwb
 if __name__ == "__main__":
 
     # Test stuff within here...
-    dataDir = "../testrecording_2021-11-09_17-06-14/"
-    outFile = "/mnt/hpx/home/fuertingers/tt2.nwb"
+    dataDir = f"/cs/home/{getpass.getuser()}/Data/oephys2nwb"
+    testDir = os.path.join(dataDir, "modtestrecording_2021-11-09_17-06-14")
+    # testDir = os.path.join(dataDir, "testrecording_2021-11-09_17-06-14")
+    outFile = os.path.join(dataDir, "test_out.nwb")
+    if os.path.isfile(outFile):
+        os.unlink(outFile)
 
-    export2nwb(dataDir, outFile, trial_markers=[3000, 3090])
-
-    nwbio = NWBHDF5IO(outFile, "r", load_namespaces=True)
-    nwbfile = nwbio.read()
-
-    tstart = nwbfile.epochs['start_time'].data[()]
-
-
-
-
+    export2nwb(testDir, outFile)
